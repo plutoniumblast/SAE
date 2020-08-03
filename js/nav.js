264218143db07fce467318ddb34212af8623e0d1
Shivesh Sinha <RealShivesh@users.noreply.github.com>
2020-08-03 19:32:21 +0530

    Frk update

--- js/nav.js
+++ js/nav.js
@@ -6,12 +6,18 @@ document.getElementById( 'nav' ).innerHTML = `
             <img class="golgol" src="../assets/nav/steer.png" style="filter: invert(100%);"/>
       </label>
 </div>
-
 <div class="nav-links">
       <a href="./index.html" target="_blank">Home</a>
       <a href="./sponsors.html" target="_blank">Sponsors</a>
       <a href="./alumni.html" target="_blank">Alumni</a>
       <a href="./cars.html" target="_blank">Our Cars</a>
       <a href="./contact.html" target="_blank">Contact Us</a>
+</div>
+<div class="sidenav">
+      <a href="./cars.html#1">SEIG 1.0</a>
+      <a href="./cars.html#2">SEIG 2.0</a>
+      <a href="./cars.html#3">BAJA</a>
+      <a href="./cars.html#4">SUPRA</a>
+      <a href=./cars.html#5">AERO</a>
 </div>
 `
